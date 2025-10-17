f930edb06ee41e71d364c0d0a12536bd817f32bc
hatopoppoo <bnt-000-smg@e-net.nara.jp>
2025-10-17 06:20:13 +0000

    プログラムの分割 敵の出現

---
+++ game/js/main.js
@@ -0,0 +1,76 @@
+
+import { player , initPlayer, drawPlayer } from "./player.js";
+import { spawnEnemy, enemies } from "./enemies.js";
+
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+initPlayer(canvas);
+spawnEnemy(canvas);
+
+const bullets = [];
+const BULLET_SPEED = -5;
+
+function tryShoot() {
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        width: 10,
+        height: 10,
+        vy: BULLET_SPEED,
+    })
+}
+
+//fillRect(x座標(横),y座標(縦),横幅,縦幅)
+
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        if (player.x > 10) {
+            player.x -= 10;
+            player.x -= 10;
+        }
+    } else if (e.key === "ArrowRight") {
+        player.x += 10;
+    }
+    else if (e.code === "Space") {
+        tryShoot();
+    }
+});
+
+function update() {
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        const bullet = bullets[i];
+        bullet.y += bullet.vy;
+        if (bullet.y < 0) {
+            bullets.splice(i, 1)
+        }
+    }
+}
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+   drawPlayer(ctx);
+
+    ctx.fillStyle = "white";
+    for (let i = bullets.length - 1; i >= 0; i--) {
+        const bullet = bullets[i];
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    }
+
+     ctx.fillStyle = "red";
+    for (let i = 0; i < enemies.length; i++) {
+        const enemy = enemies[i];
+        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    }
+}
+
+function gameLoop() {
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
